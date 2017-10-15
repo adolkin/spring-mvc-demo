7a052971a49fda2be9b89722d183e38deb1b4be8
mradol <tuanbk189@gmail.com>
2017-10-15 16:34:12 +1100

    S14: Checkboxes

--- WebContent/WEB-INF/view/student-confirmation.jsp
+++ WebContent/WEB-INF/view/student-confirmation.jsp
@@ -1,4 +1,4 @@
-<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
 <!DOCTYPE html>
 
@@ -19,5 +19,16 @@ Country: ${student.country}
 <br><br>
 
 Favourite Language: ${student.favouriteLanguage}
+
+<br><br>
+
+Operating Systems:
+
+<ul>
+	<c:forEach var="temp" items="${student.operatingSystems}">
+	<li> ${temp} </li>
+	</c:forEach>
+</ul>
+
 </body>
 </html>
--- src/com/luv2code/springdemo/mvc/Student.java
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -11,6 +11,8 @@ public class Student {
 	
 	private String favouriteLanguage;
 	
+	private String[] operatingSystems;
+	
 	public Student() {
 	}
 
@@ -44,6 +46,14 @@ public class Student {
 
 	public void setFavouriteLanguage(String favouriteLanguage) {
 		this.favouriteLanguage = favouriteLanguage;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
 	}
 	
 	
